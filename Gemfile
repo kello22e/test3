4c32bc4f089718bf31d0adcfb45d48befe4cb0c7
Emily Kellogg <kello22e@mtholyoke.edu>
2018-07-19 15:42:01 -0500

    first commit

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+ruby '2.3.7'
+
+gem 'sinatra', '~>1.4.4'
+
+#gem 'thin'
+# A sample Gemfile
---
+++ app.rb
@@ -0,0 +1,11 @@
+require 'sinatra'
+
+#require 'bundler'
+#Bundler.require
+class App < Sinatra::Base
+
+  get '/' do
+    erb :index
+  end
+
+end
